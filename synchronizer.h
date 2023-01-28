c817ed4dc8aa34b5ae4423ca0004630d35ebb4eb
Alexey Kucherenko <lehich-96@mail.ru>
2023-01-29 02:03:55 +0300

    Improve synchronizer.h

--- synchronizer.h
+++ synchronizer.h
@@ -14,35 +14,21 @@ template <typename Data> class synchronizer_t : noncopyable_t {
 public:
   explicit synchronizer_t(Data *ptr = new Data{}) : ptr_(ptr) { assert(ptr_); }
 
-  template <typename T> class unique_access : utils::noncopyable_t {
+  template <typename Type, template<typename> typename Lock>
+  class accessor : utils::noncopyable_t {
   public:
-    unique_access(T *value, std::shared_mutex &mtx)
-        : value_(value), lock_(mtx) {}
-    T *operator->() { return value_; }
-    T &operator*() { return *value_; }
+    accessor(Type *value, std::shared_mutex &mtx)
+      : value_(value), lock_(mtx) {}
+    Type *operator->() { return value_; }
+    Type &operator*() { return *value_; }
 
   private:
-    T *value_;
-    std::unique_lock<std::shared_mutex> lock_;
+    Type *value_;
+    Lock<std::shared_mutex> lock_;
   };
 
-  template <typename T> class shared_access : utils::noncopyable_t {
-  public:
-    shared_access(T *value, std::shared_mutex &mtx)
-        : value_(value), lock_(mtx) {}
-    T *operator->() { return value_; }
-    T &operator*() { return *value_; }
-
-  private:
-    T *value_;
-    std::shared_lock<std::shared_mutex> lock_;
-  };
-
-  auto unique() { return unique_access<Data>(ptr_.get(), mtx_); }
-  auto unique() const { return unique_access<const Data>(ptr_.get(), mtx_); }
-
-  auto shared() { return shared_access<Data>(ptr_.get(), mtx_); }
-  auto shared() const { return shared_access<const Data>(ptr_.get(), mtx_); }
+  auto unique() { return accessor<Data, std::unique_lock>(ptr_.get(), mtx_); }
+  auto shared() const { return accessor<const Data, std::shared_lock>(ptr_.get(), mtx_); }
 
 private:
   std::unique_ptr<Data> ptr_;
